82951f6c611482792894f7297e9fb0725083522d
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2015-04-03 22:19:49 +0200

    Added main.html which is the main page

--- app/app.js
+++ app/app.js
@@ -4,15 +4,19 @@ var app = angular.module('manneApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 		
-	$urlRouterProvider.otherwise("/blog");
+	$urlRouterProvider.otherwise("/main/blog");
 
 	//Set up the states
 	$stateProvider
-		.state('blog', {
+		.state('main', {
+			url: '/main',
+			templateUrl: "app/views/main.html"
+		})
+		.state('main.blog', {
 			url: "/blog",
 			templateUrl: "app/views/partials/blog.html"	
 		})
-		.state('games', {
+		.state('main.games', {
 			url: "/games",
 			templateUrl: "app/views/partials/games.html"
 		});
