53911e5737f96073dca47ebd3631f2ceee48bf7b
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2015-04-05 11:51:57 +0200

    Added express and node

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-var http = require('http');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// we are specifying the html directory as another public directory
+app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, '')));
 
 var port = process.env.PORT || 1337;
 
-http.createServer(function(req, res) {
-		res.writeHead(200, {
-				'Content-Type': 'text/plain' 
-		});
+var server = app.listen(port, function() {
+		var host = server.address().address;
+		var port = server.address().port;
 
-		res.end('Hello world\n');
-}).listen(port);
+		console.log('Example app listening at http://%s:%s', host, port);
+});
+
