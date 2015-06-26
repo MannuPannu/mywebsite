5e8b845b8817ddb433232a5d3e5ccdae681c8f18
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2015-06-26 15:46:19 +0200

    Try upload to azure

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function (callback) {
+
 	console.log("connection open!");		
 });
 
